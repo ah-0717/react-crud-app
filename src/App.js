9ddbc553a5f960b1e7a2883de4a713d4fe008d48
ah <ah@dummy.com>
2019-05-03 23:07:59 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function App() {
-  return (<h1>Hello, world.</h1>);
+  return (
+    // React.Fragment
+    <>
+      <label htmlFor="bar"></label>
+      <h1>Hello, world.</h1>
+    </>
+    );
 }
 
 export default App;
