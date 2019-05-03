a2cf240d279db1fab1e72d2b0a4502d19e2c5a4f
ah <ah@dummy.com>
2019-05-03 23:21:46 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     // React.Fragment
+//     <>
+//       <label htmlFor="bar"></label>
+//       <h1>Hello, world.</h1>
+//     </>
+//     );
+// }
+
+// 関数コンポーネント
+const App = () => {
   return (
-    // React.Fragment
-    <>
-      <label htmlFor="bar"></label>
-      <h1>Hello, world.</h1>
-    </>
-    );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>  
 }
 
 export default App;
